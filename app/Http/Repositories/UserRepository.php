8f2fc7d1c3c666bb2387ee4c1c04fa073f39eb32
sumon <18103159@iubat.edu>
2024-01-01 15:23:18 +0600

    user repository

---
+++ app/Http/Repositories/UserRepository.php
@@ -0,0 +1,53 @@
+<?php
+namespace App\Http\Repositories;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UserRepository{
+
+    public function store(Request $request){
+        // dd($request->all());
+        $filename=null;
+        if($request->hasFile('image')){
+            $image=$request->file('image');
+            $filename=date('Ymdhis').'.'.$image->getClientOriginalExtension();
+            $image->storeAs('/users',$filename);
+        }
+        User::create([
+            'name'=>$request->name,
+            'role_id'=>$request->role_id,
+            'image'=>$filename,
+            'email'=>$request->email,
+            'password'=>bcrypt($request->password),
+            'phone'=>$request->contact,
+            'address'=>$request->address
+        ]);
+    }
+
+
+
+    public function update(Request $request, $id){
+        $user = User::find($id);
+        try {
+            $filename=null;
+            if($request->hasFile('image')){
+                $image=$request->file('image');
+                $filename=date('Ymdhis').'.'.$image->getClientOriginalExtension();
+                $image->storeAs('/users',$filename);
+            }
+            $user->update([
+                'name'=>$request->input('name',$user->name),
+                'role_id'=>$request->input('role_id',$user->role_id),
+                'image'=>$filename ?? $user->image,
+                'email'=>$request->input('email',$user->email),
+                'phone'=>$request->input('phone',$user->phone),
+                'address'=>$request->input('address',$user->address)
+            ]);
+            $user->save();
+                return to_route('user.index');
+            } catch (\Throwable $th) {
+                return redirect()->back();
+        }
+    }
+}
